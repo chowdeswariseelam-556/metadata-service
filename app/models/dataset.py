918e059a853d6cb6c4c1c2f4aafc6b2c56b9d9c8
chowdeswariseelam-556 <chowdeswari.seelam@gmail.com>
2026-03-04 19:45:05 +0530

    Update dataset.py

--- app/models/dataset.py
+++ app/models/dataset.py
@@ -1,1 +1,28 @@
 
+from sqlalchemy import Column, String, Enum
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+import enum
+
+class SourceType(str, enum.Enum):
+    mysql = "MySQL"
+    mssql = "MSSQL"
+    postgres = "PostgreSQL"
+
+class Dataset(Base):
+    __tablename__ = "datasets"
+
+    fqn = Column(String(255), primary_key=True)
+    source_type = Column(Enum(SourceType), nullable=False)
+
+    upstream = relationship(
+        "Lineage",
+        foreign_keys="Lineage.downstream_fqn",
+        back_populates="downstream",
+    )
+
+    downstream = relationship(
+        "Lineage",
+        foreign_keys="Lineage.upstream_fqn",
+        back_populates="upstream",
+    )
