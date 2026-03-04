e3a6721ddf5421806a2d78c6c6cb3b62478755ff
chowdeswariseelam-556 <chowdeswari.seelam@gmail.com>
2026-03-04 19:46:01 +0530

    Update dataset.py

--- app/schemas/dataset.py
+++ app/schemas/dataset.py
@@ -1,1 +1,11 @@
 
+from pydantic import BaseModel
+from typing import List
+
+class DatasetCreate(BaseModel):
+    fqn: str
+    source_type: str
+
+class LineageCreate(BaseModel):
+    upstream_fqn: str
+    downstream_fqn: str
