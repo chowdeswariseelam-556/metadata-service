86bffa353f28aa4a33f7fbb339225dec185c2369
chowdeswariseelam-556 <chowdeswari.seelam@gmail.com>
2026-03-04 19:47:29 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
 
+from fastapi import FastAPI
+from app.db.session import engine
+from app.db.base import Base
+from app.api.routes import router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Metadata Service")
+
+app.include_router(router)
